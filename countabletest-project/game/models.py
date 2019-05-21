8949fb048b08350548cd83b6d371fc7383b47ee2
DenisCalixto <deniscalixto@gmail.com>
2019-05-21 00:30:12 -0700

    Home page and detail styling

--- countabletest-project/game/models.py
+++ countabletest-project/game/models.py
@@ -4,13 +4,19 @@ class Game(models.Model):
 
     creation_date = models.DateTimeField()
 
-    # not the best solution but I ran out of time andcould not create lists
+    # not the best solution but I ran out of time and could not create lists
     # I tried to focus on the rest of the requirements
+
     phrase01 = models.TextField(null=True)
+
     image01 = models.ImageField(null=True, upload_to='images/')
+
     phrase02 = models.TextField(null=True)
+
     image02 = models.ImageField(null=True, upload_to='images/')
+
     phrase03 = models.TextField(null=True)
+
     image03 = models.ImageField(null=True, upload_to='images/')
 
     def __str__(self):
--- countabletest-project/game/views.py
+++ countabletest-project/game/views.py
@@ -8,15 +8,15 @@ def home(request):
 
 def detail(request, game_id):
     game = get_object_or_404(Game, pk=game_id)
-    if game.phrase03: # if third phrase exists, this is the fifth move. One to go
+    if game.phrase03: # if third phrase exists, this is the sixth move. One to go
         current_move = 'image'
-    elif game.image02:  # if second image exists, this is the forth move. Two to go
+    elif game.image02:  # if second image exists, this is the fifth move. Two to go
         current_move = 'phrase'
-    elif game.phrase02: # if second phrase exists, this is the third move.
+    elif game.phrase02: # if second phrase exists, this is the forth move.
         current_move = 'image'
-    elif game.image01: # if first image exists, this is the second move.
+    elif game.image01: # if first image exists, this is the third move.
         current_move = 'phrase'
-    else:
+    else: # this is the second move.
         current_move = 'image'
     return render(request,'game/detail.html', {'game':game,'current_move':current_move})
 
@@ -25,26 +25,26 @@ def create(request):
         if request.POST['phrase']:
             game = Game()
             game.creation_date = timezone.datetime.now()
-            game.phrase01 = request.POST['phrase']
+            game.phrase01 = request.POST['phrase'] # creating the game already with the first move taken
             game.save()
             return redirect('/game/' + str(game.id)) # once created, show game's details
 
 def savemove(request, game_id):
     if request.method == 'POST':
         game = get_object_or_404(Game, pk=game_id)
-        if game.phrase03: # if third phrase exists, this is the fifth move. One to go
+        if game.phrase03: # if third phrase exists, this is the sixth move. One to go
             game.image03 = request.FILES['image']
             game.save()
-        elif game.image02:  # if second image exists, this is the forth move. Two to go
+        elif game.image02:  # if second image exists, this is the fifth move. Two to go
             game.phrase03 = request.POST['phrase']
             game.save()
-        elif game.phrase02: # if second phrase exists, this is the third move.
+        elif game.phrase02: # if second phrase exists, this is the forth move.
             game.image02 = request.FILES['image']
             game.save()
-        elif game.image01: # if first image exists, this is the second move.
+        elif game.image01: # if first image exists, this is the third move.
             game.phrase02 = request.POST['phrase']
             game.save()
-        else:
+        else: # this is the second move.
             game.image01 = request.FILES['image']
             game.save()
 
